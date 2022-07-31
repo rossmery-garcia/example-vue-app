a5204b347d69b431c19c69babb15e21648ac8a67
rossmery-garcia <garciarossmery@gmail.com>
2022-07-30 21:19:55 -0300

    Component Navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="container">
+  <Navbar />
+</div>
 </template>
 
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import Navbar from './components/Navbar.vue';
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      Navbar
+    }
+  })
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+    <nav>
+        <ul class="links-content">
+            <li>
+                <a href="/" class="link">Home</a>
+            </li>
+            <li>
+                <a href="/members" class="link">Members</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'Navbar'
+    })
+</script>
+
+<style>
+    nav {
+        width: 360px;
+        height: 60px;
+        display: flex;
+        background: #f4f3ee;
+        margin: 40px;
+    }
+
+    .links-content {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .links-content li {
+        list-style: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .links-content li a {
+        font-size: 24px;
+        font-weight: 600;
+        color: #eccbd9;
+        color: #f49cbb;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    .links-content li a:hover {
+        color: #f26a8d;
+    }
+
+    @media(min-width: 500px) {
+        nav {
+            width: 490px;
+        }
+    }
+
+    @media(min-width: 770px) {
+        nav {
+            width: 760px;
+        }
+    }
+</style>
